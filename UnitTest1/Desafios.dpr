56f3704f95096678b6b0731a0586b2dada33a283
Yuri <sanfordyuri@gmail.com>
2021-12-23 16:54:51 -0300

    Desafio 01 finalizado

---
+++ UnitTest1/Desafios.dpr
@@ -0,0 +1,27 @@
+program Desafios;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  Desafio01 in 'Desafio01.pas',
+  TestDesafio01 in 'TestDesafio01.pas';
+
+{$R *.RES}
+
+begin
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
